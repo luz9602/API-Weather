9956a45579ea3d5ecf5a348efdf1a37846c5ef2f
Luz Mora <luz9602@gmail.com>
2025-11-23 19:49:57 -0600

    feat: creating my documents

---
+++ script.js
@@ -0,0 +1,26 @@
+console.log("Hola api clima");
+
+async function fetchWeatherData( latitude, longitude) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true`;
+    const response = await fetch(url);
+    console.log(response);
+    const data = await response.json();
+    console.log(data);
+    console.log(data.elevation);
+    console.log(data.current_weather);
+    console.log(data.current_weather.temperature);
+    return data.current_weather;
+}
+// fetchWeatherData( 25.666815, -100.28233);
+async function handleFetchClick(){
+    console.log("Boton fetch clickeado");
+    const latitude = document.getElementById("latitude-input").value;
+    const longitude = document.getElementById("longitude-input").value;
+    const currentTemperature = document.getElementById("temp-display");
+    const wind = document.getElementById("wind-display")
+
+    const currentWeather = await fetchWeatherData(latitude, longitude);
+    currentTemperature.textContent = currentWeather.temperature;
+    wind.textContent = currentWeather.windspeed;
+
+}
